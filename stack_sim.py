8d9fa889abf1f9c6827689d72025696b184518bd
newageoflight <camint3rnal@live.com>
2020-04-29 01:05:47 +1000

    matplotlib capability added

--- stack_sim.py
+++ stack_sim.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
-# TODO: update this using matplotlib and pandas/numpy instead of this shit
+from matplotlib.ticker import PercentFormatter
 
+import matplotlib.pyplot as plt
 import random
+import re
 
 ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4])
+sanitise_filename = lambda x: re.sub(r'[<>:"/\|?*]', '', x)
+plt.style.use("ggplot")
+plt.rcParams["font.family"] = "Avenir Next"
 
 class Applicant(object):
 	"""An applicant is assumed to have two properties that count in the algorithm:
@@ -25,6 +30,9 @@ class Applicant(object):
 		self.preference_number = self.preferences.index(self.allocation)
 		# print("A category {cat} applicant has been located to {hosp}, which was their {num} preference".format(
 		# 	cat=self.category+1, hosp=self.allocation.abbreviation, num=ordinal(self.preference_number+1)))
+	def free(self):
+		self.allocation = None
+		self.preference_number = None
 
 class Hospital(object):
 	"""Hospitals are assumed to have one property that counts in the algorithm:
@@ -48,6 +56,11 @@ class Hospital(object):
 		self.spots_remaining -= len(selected_applicants)
 		for a in self.filled_spots:
 			a.allocate(self)
+	def empty(self):
+		while self.filled_spots:
+			applicant = self.filled_spots.pop()
+			applicant.free()
+			self.spots_remaining += 1
 
 hospitals = []
 with open("hospital-networks.txt", "r") as hospital_infile:
@@ -86,55 +99,101 @@ def default_stack(l):
 # applicants_stacked_with_random_first = [Applicant(push_random_to_top(stack), cat) for i in range(cat) for cat in category_counts]
 
 class Simulation(object):
-	"""docstring for Simulation"""
+	"""Runs a simulation of the allocation process"""
 	def __init__(self, starting_function):
 		self.category_counts = category_counts
 		self.hospitals = hospitals[:]
+		for hospital in self.hospitals:
+			hospital.empty()
 		self.applicants = [Applicant(starting_function(self.hospitals), cat) for cat in range(len(category_counts)) for i in range(category_counts[cat])]
-		self.runsim()
-	def runsim(self):
+		self._runsim()
+	def _runsim(self):
 		for category in range(len(self.category_counts)):
 			for hospital in self.hospitals:
 				for rank in range(len(self.hospitals)):
 					preferenced_this = list(filter(lambda a: a.preferences[rank] == hospital and a.category == category, [a for a in self.applicants if not a.allocation]))
 					hospital.fill(preferenced_this)
 		self.pprint()
-	def change_function(self, new_function):
-		self.applicants = [Applicant(new_function(self.hospitals), cat) for cat in range(len(category_counts)) for i in range(category_counts[cat])]
+	def satisfied(self, rank, category=None):
+		if category:
+			return filter(lambda a: a.preference_number == rank and a.category == category, self.applicants)
+		else:
+			return filter(lambda a: a.preference_number == rank, self.applicants)
+	def placed(self, category=None):
+		if category:
+			return filter(lambda a: a.allocation!=None and a.category==category, self.applicants)
+		else:
+			return filter(lambda a: a.allocation!=None, self.applicants)
+	def unplaced(self, category=None):
+		if category:
+			return filter(lambda a: a.allocation==None and a.category==category, self.applicants)
+		else:
+			return filter(lambda a: a.allocation==None, self.applicants)
 	def pprint(self):
-		print(sum(self.category_counts), self.category_counts)
 		for rank in range(len(self.hospitals)):
-			satisfied = list(filter(lambda a: a.preference_number == rank, self.applicants))
+			satisfied = list(self.satisfied(rank))
 			print("Total applicants who got their {ord} preference: {count} ({percent:.2%})".format(ord=ordinal(rank+1), count=len(satisfied), 
 				percent=len(list(satisfied))/sum(self.category_counts)))
-		print("Total applicants who received any placement: {count}".format(count=len(list(filter(lambda a: a.allocation!=None, self.applicants)))))
-		print("Total applicants who did not get any placement: {count}".format(count=len(list(filter(lambda a: a.allocation==None, self.applicants)))))
-		print("Total applicants: {count}".format(count=sum(self.category_counts))
+		placed = len(list(self.placed()))
+		not_placed = len(list(self.unplaced()))
+		total = sum(self.category_counts)
+		print("Total applicants who received any placement: {count} ({percent:.2%})".format(count=placed, percent=placed/total))
+		print("Total applicants who did not get any placement: {count} ({percent:.2%})".format(count=not_placed, percent=not_placed/total))
+		print("Total applicants: {count}".format(count=sum(self.category_counts), percent=placed/total))
 		for category in range(len(self.category_counts)):
 			for rank in range(len(self.hospitals)):
 				satisfied = list(filter(lambda a: a.preference_number == rank and a.category == category, self.applicants))
 				print("Total Category {cat} applicants who got their {ord} preference: {count} ({percent:.2%})".format(ord=ordinal(rank+1), count=len(satisfied), 
 					percent=len(satisfied)/category_counts[category], cat=category+1))
-			print("Total Category {cat} applicants who received any placement: {count}".format(cat=category+1, count=len(list(filter(lambda a: a.allocation!=None and a.category==category, self.applicants)))))
-			print("Total Category {cat} applicants who did not get any placement: {count}".format(cat=category+1, count=len(list(filter(lambda a: a.allocation==None and a.category==category, self.applicants)))))
-			print("Total Category {cat} applicants: {count}".format(cat=category+1, count=self.category_counts[category]))
+			cat_placed = len(list(self.placed(category)))
+			cat_not_placed = len(list(self.unplaced(category)))
+			cat_total = self.category_counts[category]
+			print("Total Category {cat} applicants who received any placement: {count} ({percent:.2%})".format(cat=category+1, count=cat_placed, percent=cat_placed/cat_total))
+			print("Total Category {cat} applicants who did not get any placement: {count} ({percent:.2%})".format(cat=category+1, count=cat_not_placed, percent=cat_not_placed/cat_total))
+			print("Total Category {cat} applicants: {count}".format(cat=category+1, count=cat_total))
+	def plot_one(self, cat=None, title_prepend=""):
+		fig, ax = plt.subplots()
+		ax.yaxis.set_major_formatter(PercentFormatter())
+		append_str = " (category {0})".format(cat+1) if cat!=None else ""
+		title = title_prepend + "Satisfied applicants" + append_str
+		ranks = list(map(ordinal, range(1, len(self.hospitals)+1)))
+		# print(ranks)
+		x_posns = range(len(ranks))
+		satisfied_ranks = [100*len(list(self.satisfied(rank, cat)))/sum(self.category_counts) for rank in range(len(self.hospitals))]
+		bars = plt.bar(x_posns, satisfied_ranks, color="green")
+		for rect in bars:
+			height = rect.get_height()
+			plt.text(rect.get_x() + rect.get_width()/2.0, height, '{:.2%}'.format(height/100), ha='center', va='bottom', fontsize='8')
+		plt.xlabel("Applicants who got their nth preference")
+		plt.xticks(x_posns, ranks)
+		plt.ylabel("%")
+		plt.title(title)
+		plt.tight_layout()
+		# print(title+".png")
+		plt.savefig(sanitise_filename(title+".png"), dpi=300)
+		# plt.show()
+	def plot_all(self, title_prepend=""):
+		for i in [None]+list(range(len(self.category_counts))):
+			self.plot_one(i, title_prepend)
 
-# bug: sims will not run separately, all 3 instances affect the same "hospitals" list
 
-# print("""
-# If all applicants select completely randomly:
-# ------------------
-# """)
-# applicants_all_random = Simulation(shuffle)
+print("""
+If all applicants select completely randomly:
+------------------
+""")
+applicants_all_random = Simulation(shuffle)
+applicants_all_random.plot_all("All random: ")
 
-# print("""
-# If all applicants stack:
-# ------------------
-# """)
-# applicants_all_stacked = Simulation(default_stack)
+print("""
+If all applicants stack:
+------------------
+""")
+applicants_all_stacked = Simulation(default_stack)
+applicants_all_stacked.plot_all("All stack: ")
 
 print("""
 If all applicants stack but move a random hospital to first:
 ------------------
 """)
 applicants_stacked_with_random_first = Simulation(push_random_to_top)
+applicants_stacked_with_random_first.plot_all("All stack with random top: ")
